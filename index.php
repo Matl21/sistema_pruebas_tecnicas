97ffff6d7ab88211cdbba8aaffdbb5d63ba05f82
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-04-05 08:51:54 -0600

    Todo local

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <!-- Icono del proyecto --->
     <link rel="shortcut icon" type="image/png" href="public/img/icons/quiz.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/bootstrap-4.3.1-css/bootstrap.min.css" >
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/css/bootstrapValidator.min.css"/>
     <!-- FontAwosome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/fontawesome-free-5.8.1-web/css/all.css" >
     
     <link rel="stylesheet" href="public/css/animate.css">
     <!-- Original Estilo-->
@@ -127,8 +127,8 @@
         </div>
         -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="public/js/popper.min.js" ></script>
+    <script src="public/js/bootstrap-4.3.1-js/bootstrap.min.js" ></script>
     <script src="public/js/menu.js"></script>
     <script src="public/js/set-datos.js"></script>
     
--- pages/examenes/steps_html.php
+++ pages/examenes/steps_html.php
@@ -393,7 +393,7 @@ if (isset($_POST['btn_enviar'])) {
     </div>
     </form>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="public/js/query-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
